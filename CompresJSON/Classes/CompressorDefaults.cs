0c024b13ed1ba5990c2ffd451856635eaa3cfc8d
a1exb1 <alex_bechmann@hotmail.com>
2015-05-02 00:03:00 +0200

    init

---
+++ CompresJSON/Classes/CompressorDefaults.cs
@@ -0,0 +1,16 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using System.IO.Compression;
+using System.Text;
+
+namespace CompresJSON
+{
+    public class CompressorDefaults
+    {
+        public static CompressionMethod compressionMethod = CompressionMethod.LZ77;
+        public static EncodingMethod encodingMethod = EncodingMethod.Base64;
+        public static Encoding encoder = UTF8Encoding.UTF8;
+    }
+}
---
+++ CompresJSON/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CompresJSON.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public JsonResult Index()
+        {
+            var rc = new Dictionary<string, object>();
+
+            var str = "akjsfl;asjdf;lasjd fl;jaskldfjalsdjfklsjfdklsjfklsfdjl";
+
+            var result = Compressor.Compress(str);
+            var decoded = Compressor.Decompress(result);
+
+            var test1 = Compressor.Compress(str, CompressionMethod.LZ77, EncodingMethod.Base64);
+            var test2 = Compressor.Compress(str, CompressionMethod.LZ77, EncodingMethod.UTF8);
+
+            rc[test1.encodingMethod.ToString()] = test1.encodedOutput;
+            rc[test2.encodingMethod.ToString()] = test2.encodedOutput;
+
+            rc["decode1"] = Compressor.Decompress(test1).decompressedOutput;
+            rc["decode2"] = Compressor.Decompress(test2).decompressedOutput;
+
+            return Json(rc, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
